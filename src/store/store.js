a7b45df7e2b60e5274612561703c8dd02f7868e3
laihsb <114426132+laihsb@users.noreply.github.com>
2023-03-07 12:46:13 -0500

    Update store.js
    added authSlice

--- src/store/store.js
+++ src/store/store.js
@@ -6,6 +6,7 @@ import allPersonnelSlice from "../features/allPersonnelSlice";
 import specificUserSlice from "../features/specificUserSlice";
 import singleMovieSlice from "../features/singleMovieSlice";
 import cartSlice from "../features/cartSlice";
+import authSlice from "../features/authSlice";
 
 
 const store = configureStore({
@@ -17,6 +18,7 @@ const store = configureStore({
     orders: ordersSlice,
     personnel: allPersonnelSlice,
     cart: cartSlice,
+	   auth: authSlice
   },
 });
 // First dispatch when the application loads
